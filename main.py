489f58dda27117f49782589542b8cbeb46ece0bb
Mutim <taormino24@gmail.com>
2022-10-12 20:22:37 -0500

    Completed week 1 WednesdayX

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#
+# Main file that will run our program
+
+main_menu = '''
+Select from the choices below to grab a tool from the toolbox.
+    [1] - Converter
+    [2] - Option 2
+    [3] - Option 3
+'''
+
+
+def start():
+    print(main_menu)
+
+
+if __name__ == '__main__':
+    start()
